c39dbc92068503f902991ee4b90299e098a7ae8e
Aabzyx <77335648+Aabzyx@users.noreply.github.com>
2021-12-02 00:08:15 +0000

    repository creation
    Added main file with first day code

---
+++ AdventOfCode2021.py
@@ -0,0 +1,23 @@
+def Chal1_1():
+    depth = []
+    cpt = 0
+    with open('.\secondaryFiles\chall1_1.txt') as f:
+        depth = f.readlines()
+    for x in range(1, len(depth)):
+        if int(depth[x]) > int(depth[x - 1]):
+            cpt += 1
+    return cpt
+
+def Chal1_2():
+    depth = []
+    cpt = 0
+    with open('.\secondaryFiles\chall1_1.txt') as f:
+        depth = f.readlines()
+        depth = [int(x) for x in depth]
+    for x in range(len(depth) - 3):
+        if sum(depth[x : x + 3]) < sum(depth[x + 1 : x + 4]):
+            cpt += 1
+    return cpt
+
+
+print(Chal1_2())
